c90a0b74b856576fee7d0b1ea1921488253d7ba7
TomasSobota <smeky.sobota@gmail.com>
2025-08-10 20:01:48 +0200

    Updates

--- server/chunks/_/_tanstack-start-manifest_v-QSuNpGqX.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ "routes": { "__root__": { "filePath": "/home/smeky/dev/3d-globe/src/routes/__root.tsx", "children": ["/"], "preloads": ["/globe-stats/assets/main-C7HWvE7U.js"], "assets": [] }, "/": { "filePath": "/home/smeky/dev/3d-globe/src/routes/index.tsx", "assets": [], "preloads": ["/globe-stats/assets/index-CUhYieGg.js"] } }, "clientEntry": "/globe-stats/assets/main-C7HWvE7U.js" });
-
-export { tsrStartManifest };
-//# sourceMappingURL=_tanstack-start-manifest_v-QSuNpGqX.mjs.map
